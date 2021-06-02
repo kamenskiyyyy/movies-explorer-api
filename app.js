0489d4e54da3114fdeee54dfd27aa5b4a3cc4d48
Kamenev Nikolay <79969997909@mail.ru>
2021-06-02 19:24:06 +0300

    create express-server

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+const { PORT = 3000 } = process.env;
+
+const app = express();
+app.disable('x-powered-by');
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(cors({
+  origin: 'https://movies.kamenskiyyyy.nomoredomains.club',
+  credentials: true,
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT);
